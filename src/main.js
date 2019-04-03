d58189516f74b75cc46fb9aba21e0d3f480ad32d
Raphael Gruber <raphael.gruber@gmail.com>
2019-04-03 18:45:16 +0200

    removed example worker

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store/index';
-import example from 'workerize-loader!@/services/example/example';
 
 Vue.config.productionTip = false;
 
@@ -11,6 +10,3 @@ new Vue({
   store,
   render: h => h(App),
 }).$mount('#app');
-
-const inst = example();
-inst.expensive(1000).then(count => console.log(`Ran ${count} loops`));
