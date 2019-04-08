54f8d29a839fd24e607913f7a90ff6e917e2eb25
Raphael Gruber <raphael.gruber@gmail.com>
2019-04-08 08:45:53 +0200

    Uncomment cordova fetch

--- src/store/modules/darksky-APP.js
+++ src/store/modules/darksky-APP.js
@@ -1,3 +1,3 @@
-/* export default function (f) {
+export default function (f) {
    cordovaFetch('https://api.darksky.net/forecast/5fe65e2763d6ddcef87f821ebaf028be/37.8267,-122.4233').then(r => f(r.json()));
-} */
+}
