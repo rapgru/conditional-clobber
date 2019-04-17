0d4ce65d97f30705fb65acead97d043b320e0ee4
michi7801 <michael.kaltenbrunner2@sz-ybbs.ac.at>
2019-04-17 13:15:39 +0200

    Added SVGs for woman Imported them in svgs.js

--- src/services/picture-service/picture-service.js
+++ src/services/picture-service/picture-service.js
@@ -1,6 +1,6 @@
-import svgImport from 'svg.js';
-import femaleBody from 'raw-loader!@/assets/svgData/01_Charakter_Weiblich.svg'
-import femaleHair from 'raw-loader!@/assets/svgData/02_Blond.svg'
+import convertXML from 'xml-js';
+import convertCss from 'css';
+import { getSVGString } from '@/services/picture-service/svgs';
 
 /*
 This Service merges all needed SVG images into one.
@@ -9,27 +9,6 @@ It loads the pictures with a raw-loader and gives it uniq names
 If any sort of clothing is not needed the 'rendertPicture.svg();' is left blank.
 */
 
-export function renderPrediction() {
-  const svgService = svgImport();
-  const rendertPicture = svgService.svg('drawing').size(200, 600);
-
-  // Add body
-  rendertPicture.svg(femaleBody);
-
-  // Add hair from settings
-  rendertPicture.svg(femaleHair);
-
-  // Add shirt
-  rendertPicture.svg(/* Load shirt */);
-
-  // Add jacket if needed
-  rendertPicture.svg(/* Load jacket */);
-
-  // Add trousers
-  rendertPicture.svg(/* Load trousers */);
-
-  // Add shoes if needed
-  rendertPicture.svg(/* Load shoes */);
-
-  return rendertPicture;
+export function renderPrediction(prediction) {
+  return 0;
 }
---
+++ src/services/picture-service/svgs.js
@@ -0,0 +1,123 @@
+/* eslint-disable import/no-webpack-loader-syntax */
+import womanBodyBlack from 'raw-loader!@/assets/SVGs/Woman/Body/Black.svg';
+import womanBodyLightYellow from 'raw-loader!@/assets/SVGs/Woman/Body/LightYellow.svg';
+import womanBodyWhite from 'raw-loader!@/assets/SVGs/Woman/Body/White.svg';
+
+import womanDressStraplessBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Dresses/Strapless/Blue.svg';
+import womanDressStraplessRed from 'raw-loader!@/assets/SVGs/Woman/Clothes/Dresses/Strapless/Red.svg';
+import womanDressStraplessWhiteblue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Dresses/Strapless/Whiteblue.svg';
+import womanDressStraplongBlack from 'raw-loader!@/assets/SVGs/Woman/Clothes/Dresses/Straplong/Black.svg';
+import womanDressStraplongPink from 'raw-loader!@/assets/SVGs/Woman/Clothes/Dresses/Straplong/Pink.svg';
+import womanDressStraplongViolet from 'raw-loader!@/assets/SVGs/Woman/Clothes/Dresses/Straplong/Violet.svg';
+import womanDressStrapshortBlack from 'raw-loader!@/assets/SVGs/Woman/Clothes/Dresses/Strapshort/Black.svg';
+import womanDressStrapshortBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Dresses/Strapshort/Blue.svg';
+import womanDressStrapshortPink from 'raw-loader!@/assets/SVGs/Woman/Clothes/Dresses/Strapshort/Pink.svg';
+import womanDressStrapshortWhite from 'raw-loader!@/assets/SVGs/Woman/Clothes/Dresses/Strapshort/White.svg';
+
+import womanExtraBikiniBlack from 'raw-loader!@/assets/SVGs/Woman/Clothes/Extras/Bikini/Black.svg';
+import womanExtraBikiniBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Extras/Bikini/Blue.svg';
+import womanExtraBikiniPink from 'raw-loader!@/assets/SVGs/Woman/Clothes/Extras/Bikini/Pink.svg';
+import womanExtraCapBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Extras/Cap/Blue.svg';
+import womanExtraCapRed from 'raw-loader!@/assets/SVGs/Woman/Clothes/Extras/Cap/Red.svg';
+import womanExtraCapViolet from 'raw-loader!@/assets/SVGs/Woman/Clothes/Extras/Cap/Violet.svg';
+import womanExtraGloveBlack from 'raw-loader!@/assets/SVGs/Woman/Clothes/Extras/Glove/Black.svg';
+import womanExtraGloveBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Extras/Glove/Blue.svg';
+import womanExtraGloveRed from 'raw-loader!@/assets/SVGs/Woman/Clothes/Extras/Glove/Red.svg';
+
+import womanJacketSpringautumnBlack from 'raw-loader!@/assets/SVGs/Woman/Clothes/Jacket/Spring-Autumn/Black.svg';
+import womanJacketSpringautumnBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Jacket/Spring-Autumn/Blue.svg';
+import womanJacketSpringautumnGrayblue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Jacket/Spring-Autumn/Grayblue.svg';
+import womanJacketWinterBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Jacket/Winter/Blue.svg';
+import womanJacketWinterGreen from 'raw-loader!@/assets/SVGs/Woman/Clothes/Jacket/Winter/Green.svg';
+
+import womanLowerbodyColdTrousersBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Cold/Trousers/Blue.svg';
+import womanLowerbodyColdTrousersDarkblue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Cold/Trousers/Darkblue.svg';
+import womanLowerbodyColdTrousersDarkgreen from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Cold/Trousers/Darkgreen.svg';
+import womanLowerbodyColdTrousersLightblue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Cold/Trousers/Lightblue.svg';
+import womanLowerbodyColdTrousersWhite from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Cold/Trousers/White.svg';
+
+import womanLowerbodyWarmSkirtBlack from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Warm/Skirt/Black.svg';
+import womanLowerbodyWarmSkirtBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Warm/Skirt/Blue.svg';
+import womanLowerbodyWarmSkirtViolet from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Warm/Skirt/Violet.svg';
+import womanLowerbodyWarmThightskirtBlack from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Warm/Thightskirt/Black.svg';
+import womanLowerbodyWarmThightskirtBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Warm/Thightskirt/Blue.svg';
+import womanLowerbodyWarmThightskirtBrown from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Warm/Thightskirt/Brown.svg';
+import womanLowerbodyWarmTrousersBlack from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Warm/Trousers/Black.svg';
+import womanLowerbodyWarmTrousersBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Warm/Trousers/Blue.svg';
+import womanLowerbodyWarmTrousersGray from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Warm/Trousers/Gray.svg';
+import womanLowerbodyWarmTrousersLightBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Lowerbody/Warm/Trousers/Lightblue.svg';
+
+import womanShoeEveryday from 'raw-loader!@/assets/SVGs/Woman/Clothes/Shoe/Everyday/Black.svg';
+import womanShoeRain from 'raw-loader!@/assets/SVGs/Woman/Clothes/Shoe/Rubberboots/Yellow.svg';
+import womanShoeWarm from 'raw-loader!@/assets/SVGs/Woman/Clothes/Shoe/Warm/Flat.svg';
+import womanShoeCold from 'raw-loader!@/assets/SVGs/Woman/Clothes/Shoe/Winter/Boots.svg';
+
+import womanUpperbodyColdCollartopBlack from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Cold/Collartop/Black.svg';
+import womanUpperbodyColdCollartopBlueviolet from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Cold/Collartop/Blueviolet.svg';
+import womanUpperbodyColdCollartopGray from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Cold/Collartop/Gray.svg';
+import womanUpperbodyColdCollartopWhite from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Cold/Collartop/White.svg';
+import womanUpperbodyColdPulloverPink from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Cold/Pullover/Pink.svg';
+import womanUpperbodyColdPulloverRed from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Cold/Pullover/Red.svg';
+import womanUpperbodyColdPulloverTurquoise from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Cold/Pullover/Turquoise.svg';
+import womanUpperbodyColdTopRed from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Cold/Top/Red.svg';
+import womanUpperbodyColdTopTurquoise from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Cold/Top/Turquoise.svg';
+import womanUpperbodyColdTopViolet from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Cold/Top/Violet.svg';
+import womanUpperbodyColdTopWhite from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Cold/Top/White.svg';
+
+import womanUpperbodyWarmCroptopBlue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/Croptop/Blue.svg';
+import womanUpperbodyWarmCroptopDarkgreen from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/Croptop/Darkgreen.svg';
+import womanUpperbodyWarmCroptopRed from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/Croptop/Red.svg';
+import womanUpperbodyWarmStraplesstopGreen from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/Straplesstop/Green.svg';
+import womanUpperbodyWarmStraplesstopTurquoise from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/Straplesstop/Turquoise.svg';
+import womanUpperbodyWarmStraplesstopViolet from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/Straplesstop/Violet.svg';
+import womanUpperbodyStraptopLightblue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/Straptop/Lightblue.svg';
+import womanUpperbodyStraptopLightviolet from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/Straptop/Lightviolet.svg';
+import womanUpperbodyStraptopRed from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/Straptop/Red.svg';
+import womanUpperbodyStraptopWhitepink from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/Straptop/Whitepink.svg';
+import womanUpperbodyTshirtLightgray from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/T-shirt/Lightgray.svg';
+import womanUpperbodyTshirtLightblue from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/T-shirt/Lightblue.svg';
+import womanUpperbodyTshirtViolet from 'raw-loader!@/assets/SVGs/Woman/Clothes/Upperbody/Warm/T-shirt/Violet.svg';
+
+import womanHairLongFringeBlond from 'raw-loader!@/assets/SVGs/Woman/Hair/Long/Fringe/Blond.svg';
+import womanHairLongFringeBrown from 'raw-loader!@/assets/SVGs/Woman/Hair/Long/Fringe/Brown.svg';
+import womanHairLongFringeOrange from 'raw-loader!@/assets/SVGs/Woman/Hair/Long/Fringe/Orange.svg';
+import womanHairLongStraightBlond from 'raw-loader!@/assets/SVGs/Woman/Hair/Long/Straight/Blond.svg';
+import womanHairLongStraightBrown from 'raw-loader!@/assets/SVGs/Woman/Hair/Long/Straight/Brown.svg';
+import womanHairLongStraightOrange from 'raw-loader!@/assets/SVGs/Woman/Hair/Long/Straight/Orange.svg';
+import womanHairLongWavytBlond from 'raw-loader!@/assets/SVGs/Woman/Hair/Long/Wavy/Blond.svg';
+import womanHairLongWavytBrown from 'raw-loader!@/assets/SVGs/Woman/Hair/Long/Wavy/Brown.svg';
+import womanHairLongWavytOrange from 'raw-loader!@/assets/SVGs/Woman/Hair/Long/Wavy/Orange.svg';
+
+import womanHairPigtailBraidBlond from 'raw-loader!@/assets/SVGs/Woman/Hair/Pigtail/Braid/Blond.svg';
+import womanHairPigtailBraidBrown from 'raw-loader!@/assets/SVGs/Woman/Hair/Pigtail/Braid/Brown.svg';
+import womanHairPigtailBraidOrange from 'raw-loader!@/assets/SVGs/Woman/Hair/Pigtail/Braid/Orange.svg';
+import womanHairPigtailDuttBlond from 'raw-loader!@/assets/SVGs/Woman/Hair/Pigtail/Dutt/Blond.svg';
+import womanHairPigtailDuttBrown from 'raw-loader!@/assets/SVGs/Woman/Hair/Pigtail/Dutt/Brown.svg';
+import womanHairPigtailDuttOrange from 'raw-loader!@/assets/SVGs/Woman/Hair/Pigtail/Dutt/Orange.svg';
+import womanHairPigtailPigtailBlond from 'raw-loader!@/assets/SVGs/Woman/Hair/Pigtail/Pigtail/Blond.svg';
+import womanHairPigtailPigtailBrown from 'raw-loader!@/assets/SVGs/Woman/Hair/Pigtail/Pigtail/Brown.svg';
+import womanHairPigtailPigtailOrange from 'raw-loader!@/assets/SVGs/Woman/Hair/Pigtail/Pigtail/Orange.svg';
+
+import womanHairShortCurlyBlond from 'raw-loader!@/assets/SVGs/Woman/Hair/Short/Curly/Blond.svg';
+import womanHairShortCurlyBrown from 'raw-loader!@/assets/SVGs/Woman/Hair/Short/Curly/Brown.svg';
+import womanHairShortCurlyOrange from 'raw-loader!@/assets/SVGs/Woman/Hair/Short/Curly/Orange.svg';
+import womanHairShortFringeBlond from 'raw-loader!@/assets/SVGs/Woman/Hair/Short/Fringe/Blond.svg';
+import womanHairShortFringeBrown from 'raw-loader!@/assets/SVGs/Woman/Hair/Short/Fringe/Brown.svg';
+import womanHairShortFringeOrange from 'raw-loader!@/assets/SVGs/Woman/Hair/Short/Fringe/Orange.svg';
+import womanHairShortStraightBlond from 'raw-loader!@/assets/SVGs/Woman/Hair/Short/Straight/Blond.svg';
+import womanHairShortStraightBrown from 'raw-loader!@/assets/SVGs/Woman/Hair/Short/Straight/Brown.svg';
+import womanHairShortStraightOrange from 'raw-loader!@/assets/SVGs/Woman/Hair/Short/Straight/Orange.svg';
+
+const svgs = {
+  example: {
+    temp: 40,
+    type: 'example',
+    svg: example,
+  },
+};
+
+export function getSVGString(name) {
+  return svgs[name].svg;
+}
+
+export default svgs;
