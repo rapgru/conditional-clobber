4f1a0abd1afeaea54dff1de87fc38081886f30d4
Raphael Gruber <raphael.gruber@gmail.com>
2019-04-24 13:24:29 +0200

    fixed raw

--- src/store/modules/general.js
+++ src/store/modules/general.js
@@ -4,155 +4,22 @@ import { vuexNestedMutations } from 'vuex-nested-mutations';
 export default {
   state: {
     weather: {
-      currently: {
-        time: 0,
-        summary: 'none',
-        temperature: 0,
-        windSpeed: 0,
-        precipIntensity: 0,
-        precipPercentage: 0,
-        precipType: 'none',
-        icon: 'none',
-        nearestStorm: 0,
-      },
-      midday: {
-        time: 0,
-        summary: 'none',
-        temperature: 0,
-        windSpeed: 0,
-        precipIntensity: 0,
-        precipPercentage: 0,
-        precipType: 'none',
-        icon: 'none',
-        cloudCover: 0,
-      },
-      daily: {
-        time: 0,
-        summary: 'none',
-        temperatureHigh: 0,
-        temperatureHighTime: 0,
-        temperatureLow: 0,
-        temperatureLowTime: 0,
-        windSpeed: 0,
-        precipIntensity: 0,
-        precipPercentage: 0,
-        precipType: 'none',
-        icon: 'none',
-      },
-      raw: {
-        hourly: {
-          data: [{ temperature: 49 }],
-        },
+      hourly: {
+        data: [{ temperature: 49 }],
       },
     },
   },
   mutations: vuexNestedMutations({
     weather: {
-      currently: {
-        setTime(state, time) {
-          state.weather.midday.time = time;
-        },
-        setSummary(state, summary) {
-          state.weather.midday.summary = summary;
-        },
-        setTemperature(state, temperature) {
-          state.weather.midday.temperature = temperature;
-        },
-        setWindSpeed(state, windSpeed) {
-          state.weather.midday.windSpeed = windSpeed;
-        },
-        setPrecipIntensity(state, precipIntensity) {
-          state.weather.midday.precipIntensity = precipIntensity;
-        },
-        setPrecipPercentage(state, precipPercentage) {
-          state.weather.midday.precipPercentage = precipPercentage;
-        },
-        setPrecipType(state, precipType) {
-          state.weather.midday.precipType = precipType;
-        },
-        setIcon(state, icon) {
-          state.weather.midday.icon = icon;
-        },
-        setCloudCover(state, cloudCover) {
-          state.weather.midday.cloudCover = cloudCover;
-        },
-      },
-      midday: {
-        setTime(state, time) {
-          state.weather.currently.time = time;
-        },
-        setSummary(state, summary) {
-          state.weather.currently.summary = summary;
-        },
-        setTemperature(state, temperature) {
-          state.weather.currently.temperature = temperature;
-        },
-        setWindSpeed(state, windSpeed) {
-          state.weather.currently.windSpeed = windSpeed;
-        },
-        setPrecipIntensity(state, precipIntensity) {
-          state.weather.currently.precipIntensity = precipIntensity;
-        },
-        setPrecipPercentage(state, precipPercentage) {
-          state.weather.currently.precipPercentage = precipPercentage;
-        },
-        setPrecipType(state, precipType) {
-          state.weather.currently.precipType = precipType;
-        },
-        setIcon(state, icon) {
-          state.weather.currently.icon = icon;
-        },
-        setNearestStorm(state, nearestStorm) {
-          state.weather.currently.nearestStorm = nearestStorm;
-        },
-      },
-      daily: {
-        setTime(state, time) {
-          state.weather.currently.time = time;
-        },
-        setSummary(state, summary) {
-          state.weather.currently.summary = summary;
-        },
-        setTemperatureHigh(state, temperatureHigh) {
-          state.weather.currently.temperature = temperatureHigh;
-        },
-        setTemperatureHighTime(state, temperatureHighTime) {
-          state.weather.currently.temperatureHighTime = temperatureHighTime;
-        },
-        setTemperatureLow(state, temperatureLow) {
-          state.weather.currently.temperatureLow = temperatureLow;
-        },
-        setTemperature(state, temperatureLowTime) {
-          state.weather.currently.temperatureLowTime = temperatureLowTime;
-        },
-        setWindSpeed(state, windSpeed) {
-          state.weather.currently.windSpeed = windSpeed;
-        },
-        setPrecipIntensity(state, precipIntensity) {
-          state.weather.currently.precipIntensity = precipIntensity;
-        },
-        setPrecipPercentage(state, precipPercentage) {
-          state.weather.currently.precipPercentage = precipPercentage;
-        },
-        setPrecipType(state, precipType) {
-          state.weather.currently.precipType = precipType;
-        },
-        setIcon(state, icon) {
-          state.weather.currently.icon = icon;
-        },
-      },
-      raw: {
-        setRaw(state, raw) {
-          state.weather.raw = raw;
-        },
+      setRaw(state, raw) {
+        state.weather = raw;
       },
     },
   }),
   actions: {
     refreshWeather(context) {
       darksky((result) => {
-        console.log(result);
-        context.commit('weather.raw.setRaw', result);
+        context.commit('weather.setRaw', result);
       });
     },
   },
