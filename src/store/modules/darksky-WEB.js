565847d76c589a9b43067f8fde474db1db2f23fc
Raphael Gruber <raphael.gruber@gmail.com>
2019-04-24 14:11:01 +0200

    fix merge

--- src/store/modules/darksky-WEB.js
+++ src/store/modules/darksky-WEB.js
@@ -55,52 +55,6 @@ export default function (f) {
     daily: {
       summary: 'Mixed precipitation throughout the week, with temperatures falling to 39°F on Saturday.',
       icon: 'rain',
-<<<<<<< HEAD
-<<<<<<< HEAD
-      data: [{
-        time: 1509944400,
-        summary: 'Rain starting in the afternoon, continuing until evening.',
-        icon: 'rain',
-        sunriseTime: 1509967519,
-        sunsetTime: 1510003982,
-        moonPhase: 0.59,
-        precipIntensity: 0.0088,
-        precipIntensityMax: 0.0725,
-        precipIntensityMaxTime: 1510002000,
-        precipProbability: 0.73,
-        precipType: 'rain',
-        temperatureHigh: 66.35,
-        temperatureHighTime: 1509994800,
-        temperatureLow: 41.28,
-        temperatureLowTime: 1510056000,
-        apparentTemperatureHigh: 66.53,
-        apparentTemperatureHighTime: 1509994800,
-        apparentTemperatureLow: 35.74,
-        apparentTemperatureLowTime: 1510056000,
-        dewPoint: 57.66,
-        humidity: 0.86,
-        pressure: 1012.93,
-        windSpeed: 3.22,
-        windGust: 26.32,
-        windGustTime: 1510023600,
-        windBearing: 270,
-        cloudCover: 0.8,
-        uvIndex: 2,
-        uvIndexTime: 1509987600,
-        visibility: 10,
-        ozone: 269.45,
-        temperatureMin: 52.08,
-        temperatureMinTime: 1510027200,
-        temperatureMax: 66.35,
-        temperatureMaxTime: 1509994800,
-        apparentTemperatureMin: 52.08,
-        apparentTemperatureMinTime: 1510027200,
-        apparentTemperatureMax: 66.53,
-        apparentTemperatureMaxTime: 1509994800,
-      },
-=======
-=======
->>>>>>> 828a0557b42fc21a167ecbba2f382d9408189a9b
       data: [
         {
           time: 1509944400,
@@ -143,10 +97,6 @@ export default function (f) {
           apparentTemperatureMax: 66.53,
           apparentTemperatureMaxTime: 1509994800,
         },
-<<<<<<< HEAD
->>>>>>> Added refreshWeather function
-=======
->>>>>>> 828a0557b42fc21a167ecbba2f382d9408189a9b
       ],
     },
   });
