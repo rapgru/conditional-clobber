bfc8e7c0be9e8cd66771cab199a20637af8ef057
Raphael Gruber <raphael.gruber@gmail.com>
2019-05-06 20:32:44 +0200

    started settings

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from '@/store/index';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default-dark.css';
+import 'material-design-icons/iconfont/material-icons.css';
 
 Vue.config.productionTip = false;
 
--- src/views/settings/Settings.vue
+++ src/views/settings/Settings.vue
@@ -1,35 +1,83 @@
 <template>
   <div class="settings-view-root">
-    <div ref="content" class="md-layout md-alignment-center-center">
-      <div class="md-layout-item">
-        <h1 class="md-title">Settings</h1>
-      </div>
+    <div ref="content">
+      <h1 class="md-title">Settings</h1>
+
+      <md-list>
+        <md-list-item md-expand>
+          <md-icon>my_location</md-icon>
+          <span class="md-list-item-text">Position</span>
+
+          <md-list slot="md-expand">
+            <md-list-item class="md-inset">
+              <md-field>
+                <label>Latidute</label>
+                <md-input v-model="number" type="number"></md-input>
+              </md-field>
+              <md-field>
+                <label>Longitude</label>
+                <md-input v-model="number" type="number"></md-input>
+              </md-field>
+            </md-list-item>
+          </md-list>
+        </md-list-item>
+
+        <md-list-item md-expand>
+          <md-icon>ac_unit</md-icon>
+          <span class="md-list-item-text">Units</span>
+
+          <md-list slot="md-expand">
+            <md-list-item class="md-inset">Console</md-list-item>
+            <md-list-item class="md-inset">PC</md-list-item>
+            <md-list-item class="md-inset">Phone</md-list-item>
+          </md-list>
+        </md-list-item>
+
+        <md-list-item md-expand>
+          <md-icon>video_library</md-icon>
+          <span class="md-list-item-text">Video</span>
+
+          <md-list slot="md-expand">
+            <md-list-item class="md-inset">Humor</md-list-item>
+            <md-list-item class="md-inset">Music</md-list-item>
+            <md-list-item class="md-inset">Movies</md-list-item>
+            <md-list-item class="md-inset">TV Shows</md-list-item>
+          </md-list>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>shopping_basket</md-icon>
+          <span class="md-list-item-text">Shop</span>
+        </md-list-item>
+      </md-list>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'settings',
-  components: {
-  },
+  name: "settings",
+  components: {},
   mounted() {
     const hammertime = new Hammer(this.$refs.content, {});
-    hammertime.on('swipeleft', () => {
-      this.$router.push('/');
+    hammertime.on("swipeleft", () => {
+      this.$router.push("/");
     });
-  },
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-  .settings-view-root {
-    height: 100%;
-    width: 100%;
+.settings-view-root {
+  height: 100%;
+  width: 100%;
 
-    .md-layout {
-      height: 100%;
-    }
+  .md-layout {
+    height: 100%;
   }
+
+  .md-layout-item {
+    overflow: scroll;
+  }
+}
 </style>
