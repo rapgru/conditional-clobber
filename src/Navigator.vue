6b5cd931ea54c862d0a67dc99e4c9b70152afd49
Raphael Gruber <raphael.gruber@gmail.com>
2019-06-10 16:12:01 +0200

    Navigator Overlay

---
+++ src/Navigator.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="all" ref="all">
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'navigator',
+  mounted() {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.all {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+
+  height: 100vh;
+  width: 100vw;
+  opacity: 0;
+  z-index: 100;
+  pointer-events: none;
+}
+</style>
